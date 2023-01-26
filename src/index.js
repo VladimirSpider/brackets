4b262c56d6467a2f828a3f7cae87edaad23973f7
VladimirSpider <vovchikbura@mail.ru>
2023-01-26 21:22:53 +0300

    feat created draft function 'check'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  if (str.length % 2 !== 0) return false;
+  const arrayBrackets = [];
+  const arrayIndex = [];
+
+  bracketsConfig.forEach((typeBrackets) => {
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] === '|' && typeBrackets[0] === str[i] && !arrayBrackets.includes('|')) {
+        console.log(str[i]);
+        arrayBrackets.push(str[i]);
+        arrayIndex.push(i);
+      }
+      else if (str[i] !== '|' && typeBrackets[0] === str[i]) {
+        arrayBrackets.push(str[i]);
+        arrayIndex.push(i);
+      } else {
+        let endItem = arrayBrackets[arrayBrackets.length-1];
+        if (!!arrayBrackets.length && typeBrackets.includes(endItem) && str[i] === typeBrackets[1]) {
+          if ((i - arrayIndex[arrayIndex.length - 1]) % 2 !== 0) {
+            arrayBrackets.pop();
+            arrayIndex.pop();
+          }
+        }
+      }
+    }
+  });
+  console.log(arrayBrackets);
+  return !arrayBrackets.length;
 }
