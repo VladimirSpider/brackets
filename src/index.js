ea6c199506873475a6bc516399f816893d26225a
VladimirSpider <vovchikbura@mail.ru>
2023-01-26 21:42:46 +0300

    feat used forEach

--- src/index.js
+++ src/index.js
@@ -4,27 +4,27 @@ module.exports = function check(str, bracketsConfig) {
   const arrayIndex = [];
 
   bracketsConfig.forEach((typeBrackets) => {
-    for (let i = 0; i < str.length; i++) {
-      if ((str[i] === '|' && typeBrackets[0] === str[i] && !arrayBrackets.includes('|'))
-        || (str[i] === '7' && typeBrackets[0] === str[i] && !arrayBrackets.includes('7'))
-        || (str[i] === '8' && typeBrackets[0] === str[i] && !arrayBrackets.includes('8'))) {
-        arrayBrackets.push(str[i]);
-        arrayIndex.push(i);
+    str.split('').forEach((currentItem, index) => {
+      if ((currentItem === '|' && typeBrackets[0] === currentItem && !arrayBrackets.includes('|'))
+        || (currentItem === '7' && typeBrackets[0] === currentItem && !arrayBrackets.includes('7'))
+        || (currentItem === '8' && typeBrackets[0] === currentItem && !arrayBrackets.includes('8'))) {
+        arrayBrackets.push(currentItem);
+        arrayIndex.push(index);
       }
-      else if (str[i] !== '|' && str[i] !== '7' && str[i] !== '8' && typeBrackets[0] === str[i]) {
-        arrayBrackets.push(str[i]);
-        arrayIndex.push(i);
+      else if (currentItem !== '|' && currentItem !== '7' && currentItem !== '8' && typeBrackets[0] === currentItem) {
+        arrayBrackets.push(currentItem);
+        arrayIndex.push(index);
       } else {
         let endItem = arrayBrackets[arrayBrackets.length-1];
-        if (!!arrayBrackets.length && typeBrackets.includes(endItem) && str[i] === typeBrackets[1]) {
-          if ((i - arrayIndex[arrayIndex.length - 1]) % 2 !== 0) {
+        if (!!arrayBrackets.length && typeBrackets.includes(endItem) && currentItem === typeBrackets[1]) {
+          if ((index - arrayIndex[arrayIndex.length - 1]) % 2 !== 0) {
             arrayBrackets.pop();
             arrayIndex.pop();
           }
         }
       }
-    }
+    })
   });
-  console.log(arrayBrackets);
+
   return !arrayBrackets.length;
 }
